bacee0f24b62a336e58fad79db01812bb3229161
Orcan Kolankaya <orcankolankaya@gmail.com>
2020-03-22 10:32:47 +0300

    my initial commit
    Modified-by: Orcan Kolankaya <orcankolankaya@gmail.com>

---
+++ helloworld/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+void Log(const char *message);
+
+int main() {
+  std::cout << "Hi there everyone\n";
+  Log("Hello from Orcan to WORLD!! ");
+}
---
+++ mystaticlib/src/mystatic.cpp
@@ -0,0 +1,10 @@
+#include "include/mystatic.h"
+#include <iostream>
+
+namespace mystatic {
+  
+  void PrintMessage() {
+    std::cout << "Hello World from MyStatic Lib" << std::endl;
+  }
+  
+}
